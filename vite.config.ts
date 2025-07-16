852415fd6d08376bf0b887956fe336b6665875ce
badnap <badnap1@gmail.com>
2025-07-16 14:36:01 +0200

    6x

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/muath-hassan/',
+ 
   server: {
     host: "::",
     port: 8080,
@@ -20,4 +20,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/muath-hassan/',
 }));
